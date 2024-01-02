fb1ef5afc2d9768f154c0eaa4916721d59903692
fctopz <38725882+fctopz@users.noreply.github.com>
2024-01-02 16:25:06 +0000

    added object literals, alternatives for switch case statements

--- myScript.js
+++ myScript.js
@@ -9,5 +9,28 @@ class Rectangle{
         console.log("This is a rectangle! \n width:"+ this.width +" height:"+this.height+" color:"+this.color);
     }
 }
+
+function getCaffeine(type){
+    let caffeine;
+
+    const map = {
+        'Coffee':'95 mg',
+        'Redbull':'147 mg',
+        'Tea':'11 mg',
+        'Soda':'21 mg'
+    };
+    caffeine = map[type] ?? 'Not found';
+    return caffeine;
+}
+
 let r1 = new Rectangle(3,5,"blue");
 let r2 = new Rectangle(4,9,"red");
+console.log(getCaffeine('Teax'));
+
+
+/*
+practice array methods
+https://www.youtube.com/watch?v=R8rmfD9Y5-c
+
+source: https://www.youtube.com/shorts/cbA2HY1xV0w
+*/
